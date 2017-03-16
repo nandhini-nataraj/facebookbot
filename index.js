6fb97db5593c7551fe2886aaeaa5952de8c8d6a2
Nataraj Nandhini <Nataraj Nandhini>
2017-03-16 17:29:13 +0530

    updted

--- index.js
+++ index.js
@@ -35,9 +35,7 @@ app.get('/webhook/', function (req, res) {
 
 // to post data
 app.post('/webhook/', function (req, res) {
-	//messages.greetingText();
-	//console.log('greeting text executed');
-	messages.getStarted();
+	messages.greetingText();
 	
 	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
